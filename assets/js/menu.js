0a42f7307301c6669b30a2d50210db2f6574e06f
Мама <mama@yandex.ru>
2022-11-07 15:15:17 +0500

    Первая подготовка меню

---
+++ assets/js/menu.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+	$(".start_game").click(function(){
+	let name_user = $('.name').val();
+	let block_menu= $('#menu');
+	let block_game= $('#game');
+	if (name_user!="") {
+block_menu.css("display","none");
+	block_game.css("display","block");
+	Game();
+	}
+	else{
+		alert("Побежал кабанчиком имя заполнять!");
+	}
+	console.log(name_user);
+	console.log(block_menu);
+	console.log(block_game); 
+	});
+	$('.settings').click(function(){
+		let settings_window=$('.settings_window');
+		settings_window.fadeIn();
+	});
+	$('.close').click(function(){
+		let settings_window=$('.settings_window');
+		settings_window.fadeOut();
+	});
+	$('.builds li').click(function(){
+	 select_image_x=$(this).attr("x");
+	 select_image_y=$(this).attr("y");
+	 type=$(this).attr("type");
+	 $('.builds ul li img').css('background','none');
+	 $(this).find('img').css('background','red');
+	 
+	});
+});
